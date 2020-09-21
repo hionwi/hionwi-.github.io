d87b3b962369369a0dc39deabb790e4b4391d122
HE-DE <201900130012@mail.sdu.edu.cn>
2020-09-21 23:45:45 +0800

    0.2

--- backend/helloworld/helloworld/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def hello(request):
-    views_str="<a href='https://www.runoob.com/'>点击跳转</a>"
-    return render(request,'runoob.html',{"views_str":views_str})
